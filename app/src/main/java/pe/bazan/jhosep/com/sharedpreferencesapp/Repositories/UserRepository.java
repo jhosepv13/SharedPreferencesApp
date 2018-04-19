f742f8549a73b07a107eeba58a541f37c5e269e3
jhosepv13 <jhosepv13@hotmail.com>
2018-04-19 11:17:32 -0500

    Subiendo SharedPreferences

---
+++ app/src/main/java/pe/bazan/jhosep/com/sharedpreferencesapp/Repositories/UserRepository.java
@@ -0,0 +1,40 @@
+package pe.bazan.jhosep.com.sharedpreferencesapp.Repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pe.bazan.jhosep.com.sharedpreferencesapp.Models.User;
+
+/**
+ * Created by Alumno on 17/04/2018.
+ */
+
+public class UserRepository {
+
+    public static List<User> users =new ArrayList<>();
+
+    static {
+        users.add(new User(100, "ebenites", "tecsup","Erick Benites"));
+        users.add(new User(200, "jfarfan", "tecsup","Jaime Farfan"));
+        users.add(new User(300, "drodriguez", "tecsup","David Rodriguez"));
+    }
+
+    public static User login(String username, String password){
+        for(User user:users){
+            if(user.getUsername().equalsIgnoreCase(username)&&user.getPassword().equals(password)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public static User getUser(String username){
+        for(User user:users){
+            if(user.getUsername().equalsIgnoreCase(username)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+}
